f97d2a26ce5a0b4f2ba4679e99d713498e841c3a
Mikołaj Pich <m.pich@outlook.com>
2019-01-26 15:47:33 +0100

    Add support for api in another repo

--- src/content.js
+++ src/content.js
@@ -1,10 +1,13 @@
 function getLastVersionTag() {
     const versionDetails = document.querySelector(".issue-details.session-details .emphasis");
+    let version;
     if (versionDetails) {
-        return versionDetails.textContent.split(" ")[1];
+        version = versionDetails.textContent.split(" ")[1];
+    } else {
+        version = document.querySelectorAll(".build-version-table tbody tr td:nth-child(1) a")[0].textContent.split(" ")[0];
     }
 
-    return document.querySelectorAll(".build-version-table tbody tr td:nth-child(1) a")[0].textContent.split(" ")[0];
+    return version.includes('-') ? 'master' : version;
 }
 
 function getArrayOfTraceItems() {
@@ -34,17 +37,17 @@ function addLinksToTraceItems() {
 
         const span = document.createElement("span");
         const a = document.createElement("a");
-        a.setAttribute("href", "https://github.com/wulkanowy/wulkanowy/blob/"
-                + getLastVersionTag()
-                + (packageName.includes("io.github.wulkanowy.api") ? '/api' : "/app") + "/src/main/java/"
-                + getPathFromPackage(packageName) + "/"
-                + getFilename(basename));
+        a.setAttribute("href", "https://github.com/wulkanowy/"
+            + (packageName.includes("io.github.wulkanowy.api") ?
+                'api/blob/' + getLastVersionTag() + '/src/main/kotlin/' :
+                'wulkanowy/blob/' + getLastVersionTag() + '/app/src/main/java/')
+            + getPathFromPackage(packageName) + "/"
+            + getFilename(basename));
         a.textContent = " Open →";
         a.setAttribute("target", "_blank");
         span.appendChild(a);
         item.appendChild(span);
     });
-
 }
 
 const obs = new MutationObserver(function (mutations, observer) {
@@ -53,4 +56,4 @@ const obs = new MutationObserver(function (mutations, observer) {
         document.querySelector(".stacktrace").classList.add("cl-loaded");
     }
 });
-obs.observe(document.body, { childList: true, subtree: true, attributes: false, characterData: false });
+obs.observe(document.body, {childList: true, subtree: true, attributes: false, characterData: false});
